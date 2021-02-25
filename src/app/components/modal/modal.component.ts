f7d28b04361edcb0ff567d630ada5f3f64bcde7a
Giancarlo <giandest@hotmail.com>
2021-02-25 11:33:53 -0400

    Finishing the first Modal design.

--- src/app/components/modal/modal.component.ts
+++ src/app/components/modal/modal.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import { faEye } from '@fortawesome/free-solid-svg-icons';
+import { faThumbsUp } from '@fortawesome/free-solid-svg-icons';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-modal',
@@ -10,6 +13,9 @@ import { faTimes } from '@fortawesome/free-solid-svg-icons';
 export class ModalComponent implements OnInit {
 
   faTimes = faTimes;
+  faEye = faEye;
+  faThumbsUp = faThumbsUp;
+  faStar = faStar;
 
   constructor() { }
 
